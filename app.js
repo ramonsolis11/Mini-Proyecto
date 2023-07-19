52515410e3c10e9727efa131a72f0b482a54765b
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-07-19 10:50:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const port = 3000;
 // Configurar el motor de plantillas
 app.set('view engine', 'ejs');
 
+// Importar el controlador de puertas de embarque
+const puertasEmbarqueController = require('./controllers/puertasEmbarqueController');
+
 // Ruta principal
 app.get('/', (req, res) => {
     res.render('index');
-    });
+});
 
-    // Ruta para mostrar aviones
-    app.get('/aviones', (req, res) => {
+// Ruta para mostrar aviones
+app.get('/aviones', (req, res) => {
     // Obtener aviones desde una fuente de datos (p. ej., un arreglo)
     const aviones = [
         { registro: 'AB123', aerolinea: 'Airline 1', capacidad: 150, estado: 'disponible' },
@@ -20,22 +23,11 @@ app.get('/', (req, res) => {
     ];
 
     res.render('aviones', { aviones });
-    });
-
-    // Ruta para mostrar puertas de embarque
-    app.get('/puertas-embarque', (req, res) => {
-    // Obtener las puertas de embarque disponibles y ocupadas desde una fuente de datos (p. ej., un arreglo)
-    const puertasEmbarque = [
-        { numero: 'A1', disponible: true, avionAsignado: null },
-        { numero: 'A2', disponible: false, avionAsignado: { registro: 'AB123', aerolinea: 'Airline 1' } },
-        { numero: 'B1', disponible: true, avionAsignado: null },
-        { numero: 'B2', disponible: false, avionAsignado: { registro: 'CD456', aerolinea: 'Airline 2' } },
-    ];
-
-    res.render('puertasEmbarque', { puertasEmbarque });
-    });
-
-    app.listen(port, () => {
-    console.log(`Servidor web en ejecución en http://localhost:${port}`);
 });
 
+// Ruta para mostrar puertas de embarque
+app.get('/puertas-embarque', puertasEmbarqueController.obtenerPuertasEmbarque);
+
+app.listen(port, () => {
+    console.log(`Servidor web en ejecución en http://localhost:${port}`);
+});
