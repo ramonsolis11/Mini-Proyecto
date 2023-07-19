1934a2e38c2d34958b4136c31405d1875e596407
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-07-19 16:04:22 -0600

    Update

--- app.js
+++ app.js
@@ -1,32 +1,68 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+// Configuración de body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configurar el motor de plantillas
 app.set('view engine', 'ejs');
 
-// Importar el controlador de puertas de embarque
-const puertasEmbarqueController = require('./controllers/puertasEmbarqueController');
+// Importar los modelos
+const Avion = require('./models/Avion');
+const PuertaEmbarque = require('./models/PuertaEmbarque');
+
+// Array para almacenar los aviones
+const aviones = [];
+
+// Array para almacenar las puertas de embarque
+const puertasEmbarque = [];
 
 // Ruta principal
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Ruta para mostrar aviones
+// Ruta para mostrar la lista de aviones
 app.get('/aviones', (req, res) => {
-    // Obtener aviones desde una fuente de datos (p. ej., un arreglo)
-    const aviones = [
-        { registro: 'AB123', aerolinea: 'Airline 1', capacidad: 150, estado: 'disponible' },
-        { registro: 'CD456', aerolinea: 'Airline 2', capacidad: 200, estado: 'en mantenimiento' },
-        { registro: 'EF789', aerolinea: 'Airline 3', capacidad: 180, estado: 'en vuelo' }
-    ];
-
     res.render('aviones', { aviones });
 });
 
-// Ruta para mostrar puertas de embarque
-app.get('/puertas-embarque', puertasEmbarqueController.obtenerPuertasEmbarque);
+// Ruta para agregar un nuevo avión
+app.post('/aviones', (req, res) => {
+    const { registro, aerolinea, capacidad, estado } = req.body;
+
+    // Crear una instancia del avión
+    const avion = new Avion(registro, aerolinea, parseInt(capacidad), estado);
+
+    // Agregar el avión al array
+    aviones.push(avion);
+
+    // Redireccionar a la página de aviones
+    res.redirect('/aviones');
+});
+
+// Ruta para eliminar un avión por número de registro
+app.post('/aviones/delete', (req, res) => {
+    const { registro } = req.body;
+
+    // Encontrar el índice del avión en el array
+    const avionIndex = aviones.findIndex(avion => avion.registro === registro);
+
+    // Si se encontró el avión, eliminarlo del array
+    if (avionIndex !== -1) {
+        aviones.splice(avionIndex, 1);
+    }
+
+    // Redireccionar a la página de aviones
+    res.redirect('/aviones');
+});
+
+// Ruta para mostrar la lista de puertas de embarque
+app.get('/puertas-embarque', (req, res) => {
+    res.render('puertasEmbarque', { puertasEmbarque });
+});
 
 app.listen(port, () => {
     console.log(`Servidor web en ejecución en http://localhost:${port}`);
